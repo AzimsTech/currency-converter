ab808a56358229d68a3d7fa67cad8d4a83112a02
Wan Azims <azimstech@gmail.com>
2025-11-23 05:29:27 +0800

    Add fetch event listener for PWA pass-through
    Implement a fetch event listener for pass-through strategy.

--- sw.js
+++ sw.js
@@ -1,2 +1,8 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", e => e.waitUntil(clients.claim()));
+
+self.addEventListener("fetch", (event) => {
+    // This is a "pass-through" strategy. 
+    // It satisfies the PWA requirement without caching anything yet.
+    event.respondWith(fetch(event.request));
+});
